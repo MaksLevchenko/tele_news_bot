33c48c79909d8613cc1a04d4a17107cb2a3926bf
madmax484 <madmax20@bk.ru>
2022-11-10 16:15:43 +0300

    tele_news_bot

---
+++ bot.py
@@ -0,0 +1,64 @@
+import datetime
+import json
+from aiogram.utils.markdown import hbold,hunderline, hlink, hcode
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.dispatcher.filters import Text
+from config import token
+from get_news import difirense_news, get_news
+
+bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def start(messege: types.Message):
+    start_button = ["Горячие новости", "Все новости", "Последние 5 новостей"]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_button)
+    await messege.answer("Привет, свежих новостей не хочешь?", reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals="Все новости"))
+async def get_all_news(messege: types.Message):
+    get_news()
+    with open("fresh_news.json", "r", encoding="utf-8") as file:
+        new_dict = json.load(file)
+    # print(new_dict)
+    for k, v in sorted(new_dict.items()):
+        # news = f"На <b>{datetime.datetime.now().strftime('%Y-%m-%d')} {v['date']}</b>\n" \
+        #        f"<u>{v['news']}</u>\n" \
+        #        f"{v['news_link']}"
+        news = f"На {hbold(datetime.datetime.now().strftime('%Y-%m-%d'))} {hbold(v['date'])}\n" \
+               f"{hlink(v['news'], v['news_link'])}"
+        # print(news)
+        await messege.answer(news)
+
+@dp.message_handler(Text(equals="Последние 5 новостей"))
+async def get_last_five_news(messege: types.Message):
+    with open("fresh_news.json", "r", encoding="utf-8") as file:
+        new_dict = json.load(file)
+    for k, v in sorted(new_dict.items())[-5:]:
+        news = f"На {hbold(datetime.datetime.now().strftime('%Y-%m-%d'))} {hbold(v['date'])}\n" \
+               f"{hlink(v['news'], v['news_link'])}"
+
+        await messege.answer(news)
+
+@dp.message_handler(Text(equals="Горячие новости"))
+async def get_fresh_news(messege: types.Message):
+    fresh_news = difirense_news()
+
+    if len(fresh_news) >= 1:
+        for k, v in sorted(fresh_news.items()):
+            news = f"На {hbold(datetime.datetime.now().strftime('%Y-%m-%d'))} {hbold(v['date'])}\n" \
+                   f"{hlink(v['news'], v['news_link'])}"
+
+            await messege.answer(news)
+    else:
+        await messege.answer("Пока нет свежих новостей...")
+
+@dp.message_handler()
+def main():
+    executor.start_polling(dp)
+
+if __name__ == "__main__":
+    main()
---
+++ get_news.py
@@ -0,0 +1,91 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import datetime
+
+def get_news():
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+    }
+    url = r"https://lenta.ru/"
+    r = requests.get(url=url, headers=headers)
+
+    news_dict = {}
+
+    with open('index.html', 'w', encoding="utf-8") as file:
+        file.write(r.text)
+
+
+    with open("index.html", "r", encoding="utf-8") as file:
+
+        soup = BeautifulSoup(file.read(), "html.parser")
+        big_new = soup.find_all('a', {"class":"card-mini"})
+        for news in big_new[:10]:
+            date = news.text[-5:]
+            news_w = news.text[:-5]
+            news_link = "https://lenta.ru/" + news.get("href")
+            title_news = news.get('href').split('/')[-2]
+
+            news_dict[title_news] = {
+                "date": date,
+                "news": news_w,
+                "news_link": news_link
+            }
+    with open("fresh_news.json", "w", encoding="utf-8") as file:
+        json.dump(news_dict, file, indent=4, ensure_ascii=False)
+
+def difirense_news():
+    with open("fresh_news.json", "r", encoding="utf-8") as file:
+        news_dict = json.load(file)
+    # print(news_list)
+    headers = {
+        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+    }
+    url = r"https://lenta.ru/"
+    r = requests.get(url=url, headers=headers)
+
+    fresh_news = {}
+
+    with open('index.html', 'w', encoding="utf-8") as file:
+        file.write(r.text)
+
+
+    with open("index.html", "r", encoding="utf-8") as file:
+
+        soup = BeautifulSoup(file.read(), "html.parser")
+        big_new = soup.find_all('a', {"class":"card-mini"})
+        for news in big_new[:10]:
+            title_news = news.get('href').split('/')[-2]
+            if title_news in news_dict:
+                continue
+            else:
+                date = news.text[-5:]
+                news_w = news.text[:-5]
+                news_link = "https://lenta.ru/" + news.get("href")
+                title_news = news.get('href').split('/')[-2]
+
+                news_dict[title_news] = {
+                    "date": date,
+                    "news": news_w,
+                    "news_link": news_link
+                }
+
+                fresh_news[title_news] = {
+                    "date": date,
+                    "news": news_w,
+                    "news_link": news_link
+                }
+
+    with open("fresh_news.json", "w", encoding="utf-8") as file:
+        json.dump(fresh_news, file, indent=4, ensure_ascii=False)
+    return fresh_news
+
+
+def main():
+    get_news()
+    # difirense_news()
+
+
+if __name__ == "__main__":
+    main()
